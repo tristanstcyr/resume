75fdf51d91114417efa844dfa59b7dee521c4339
Tristan St-Cyr <tristan.stcyr@gmail.com>
2016-10-22 19:34:23 -0700

    Initial

---
+++ data.js
@@ -0,0 +1,57 @@
+var data = {
+    "subject" : {
+        "full-name" : "Tristan St-Cyr",
+        "email" : "tristan.stcyr@gmail.com",
+        "phone" : "425-224-5268",
+    },
+    "industry-experience" : [ {
+            "role" : "Senior Software Engineer",
+            "start-date" : "September 2011",
+            "end-date" : "Present",
+            "location" : "Redmond, WA, USA",
+            "institution" : "Microsoft",
+            "description" : "Developer on a distributed query engine used in several Microsoft products including Excel, Power BI, Azure ML, Azure Data Factory, and more",
+            "achievements" : [
+                "End-to-end development and major contributions to several data import features: ODBC, ADO.NET, OLE DB, SAP HANA, SAP Business Objects, Microsoft Analysis Services, Active Directory, Google Analytics, Lithium, Snowflake, Amazon Redshift, and more.",
+                "Developed Windows installer using WIX toolkit for Power Query.",
+                "Participated in mentoring junior coworkers and interviewing candidates.",
+                "Ensured that security requirements were met."
+            ]
+        }    ],
+    "education" : [
+        {
+           "institution" : "Concordia University",
+           "location" : "Montreal, QC, CA",
+           "formation" : "Bachelor of Engineering, Software Engineering",
+           "start-date" : "2007",
+           "end-date" : "2011",
+           "note" : "Graduated with distinction"
+        }
+    ],
+    "internships" : [
+        {
+            "role" : "Software Development Engineer Intern",
+            "technologies" : "C#, Visual Studio 2010, CLR/IL",
+            "location" : "Redmond, WA, USA",
+            "start-date" : "May 2010",
+            "end-date" : "July 2010",
+            "institution" : "Microsoft",
+            "description" : "Developed data model and database loader for .NET PDB files."
+        }, {
+            "role" : "Research Intern",
+            "technologies" : "Java, Eclipse, Eclipse Java Development Tools (JDT)",
+            "start-date" : "May 2009",
+            "end-date" : "August 2009",
+            "institution" : "Concordia University",
+            "description" : "Integration of Java Modeling Language, Eclipse, and Microsoft Boogie into an Eclipse plugin."
+        }, {
+            "role" : "Engineer Intern",
+            "technologies" : "C++, XML, Visual Studio 2008",
+            "location" : "Montreal, QC, CA",
+            "start-date" : "September 2008",
+            "end-date" : "December 2008",
+            "institution" : "CAE",
+            "description" : "Development of helicopter Heads-up display."
+        }
+    ]
+};
