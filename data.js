92c09999ae16ef6c4b8b1e9141850b2daf86ddbb
Tristan St-Cyr <tristan.stcyr@gmail.com>
2016-10-23 19:56:59 -0700

    Many changes in content, style. Still in draft.

--- data.js
+++ data.js
@@ -3,6 +3,7 @@ var data = {
         "full-name" : "Tristan St-Cyr",
         "email" : "tristan.stcyr@gmail.com",
         "phone" : "425-224-5268",
+        "about" : "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut facilisis tincidunt tortor, non feugiat mauris euismod id. Phasellus eleifend, sem sed dictum lobortis, eros urna mollis ex, quis sagittis lacus tortor nec dui. Integer molestie leo ante, quis laoreet metus faucibus non. Vestibulum laoreet fringilla sapien, ultrices vehicula erat tempor in."
     },
     "industry-experience" : [ {
             "role" : "Senior Software Engineer",
@@ -10,7 +11,7 @@ var data = {
             "end-date" : "Present",
             "location" : "Redmond, WA, USA",
             "institution" : "Microsoft",
-            "description" : "Developer on a distributed query engine used in several Microsoft products including Excel, Power BI, Azure ML, Azure Data Factory, and more",
+            "description" : "Developer on a distributed query engine used in several Microsoft products including Excel, Power BI, Azure ML, Azure Data Factory, and more.",
             "achievements" : [
                 "End-to-end development and major contributions to several data import features: ODBC, ADO.NET, OLE DB, SAP HANA, SAP Business Objects, Microsoft Analysis Services, Active Directory, Google Analytics, Lithium, Snowflake, Amazon Redshift, and more.",
                 "Developed Windows installer using WIX toolkit for Power Query.",
@@ -26,32 +27,50 @@ var data = {
            "start-date" : "2007",
            "end-date" : "2011",
            "note" : "Graduated with distinction"
+        }, {
+            "institution" : "Conrodia University",
+            "location" : "Montreal, QC, CA",
+            "formation" : "Bachelor of Fine Arts, Design Arts",
+            "start-date" : "2004",
+            "end-date" : "2006",
+            "note" : "Completed 45 out of 60 credits"
         }
     ],
     "internships" : [
         {
             "role" : "Software Development Engineer Intern",
             "technologies" : "C#, Visual Studio 2010, CLR/IL",
-            "location" : "Redmond, WA, USA",
+            "location" : "Redmond, Washington",
             "start-date" : "May 2010",
             "end-date" : "July 2010",
             "institution" : "Microsoft",
             "description" : "Developed data model and database loader for .NET PDB files."
         }, {
             "role" : "Research Intern",
+            "location" : "Montreal, Quebec",
             "technologies" : "Java, Eclipse, Eclipse Java Development Tools (JDT)",
             "start-date" : "May 2009",
             "end-date" : "August 2009",
             "institution" : "Concordia University",
-            "description" : "Integration of Java Modeling Language, Eclipse, and Microsoft Boogie into an Eclipse plugin."
+            "description" : "Integration of Java Modeling Language, Eclipse, and Microsoft Boogie into an Eclipse plugin.",
+            "link" : "https://sourceforge.net/p/jmlspecs/wiki/JML4/"
         }, {
             "role" : "Engineer Intern",
             "technologies" : "C++, XML, Visual Studio 2008",
-            "location" : "Montreal, QC, CA",
+            "location" : "Montreal, Quebec",
             "start-date" : "September 2008",
             "end-date" : "December 2008",
             "institution" : "CAE",
-            "description" : "Development of helicopter Heads-up display."
+            "description" : "Development on helicopter heads-up display."
         }
-    ]
+    ],
+    "links" : [
+        "https://github.com/tristanstcyr",
+        "https://www.linkedin.com/in/tristanstcyr"
+    ],
+    "patents" : {
+        "title" : "Constructing Queries for Execution Over Multi-Dimensional Data Structures",
+        "publication-date" : "June 2015",
+        "publication-number" : "20150178407"
+    }
 };
